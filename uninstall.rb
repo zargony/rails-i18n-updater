88b04456ca26b946632939e8ee9257f86e90adc4
Andreas Neuhaus <zargony@zargony.com>
2009-08-11 13:04:19 +0200

    Print note about removing downloaded locales upon uninstall

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,2 @@
-# Uninstall hook code here
+require File.join(File.dirname(__FILE__), 'lib', 'rails_i18n_updater', 'config')
+puts "You may also want to remove downloaded locales from #{RailsI18nUpdater::Config.local_path}"
