5189edeea67e15f3f0da7eb3e5888e20bc5717c7
Andreas Neuhaus <zargony@zargony.com>
2011-03-03 10:34:22 +0100

    Escape paths in shell commands

--- lib/rails_i18n_updater/tasks.rb
+++ lib/rails_i18n_updater/tasks.rb
@@ -2,15 +2,16 @@ namespace :i18n do
   desc 'Update (download) Rails core translations'
   task :update do
     require 'rails_i18n_updater/config'
+    require 'shellwords'
     temp_path = "#{RailsI18nUpdater::Config.local_path}.tmp"
     sh <<-EOC
-      rm -rf #{temp_path} && \
-      git clone -q --depth 1 #{RailsI18nUpdater::Config.repository_url} #{temp_path} && \
-      cd #{temp_path} && \
+      rm -rf #{temp_path.shellescape} && \
+      git clone -q --depth 1 #{RailsI18nUpdater::Config.repository_url} #{temp_path.shellescape} && \
+      cd #{temp_path.shellescape} && \
       git checkout -q #{RailsI18nUpdater::Config.repository_branch} && \
-      rm -rf #{RailsI18nUpdater::Config.local_path} && \
-      mv #{File.join(temp_path, RailsI18nUpdater::Config.repository_path)} #{RailsI18nUpdater::Config.local_path} && \
-      rm -rf #{temp_path}
+      rm -rf #{RailsI18nUpdater::Config.local_path.shellescape} && \
+      mv #{File.join(temp_path, RailsI18nUpdater::Config.repository_path).shellescape} #{RailsI18nUpdater::Config.local_path.shellescape} && \
+      rm -rf #{temp_path.shellescape}
     EOC
   end
 end
